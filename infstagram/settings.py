289b1c4184e9737dea53ee0557545734d3e8e258
Martina Sirotic <sirotic.martina@gmail.com>
2022-02-19 13:02:36 +0100

    registracija

--- infstagram/settings.py
+++ infstagram/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&f9xj0!kg^j+w(yzot#3lddw%!a&#-r@y^y9nrg^*ur*e@$*wv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
