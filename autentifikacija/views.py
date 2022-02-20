c6058758602ed6747854a5f1eaf239c25567c676
Toni Baskijera <tonibaskijera@gmail.com>
2022-02-20 11:04:12 +0100

    brisanje

--- autentifikacija/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import *
-
-
-# Create your views here
--- infstagram/settings.py
+++ infstagram/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'autentifikacija.apps.AutentifikacijaConfig',
     'crispy_forms'
 ]
 
