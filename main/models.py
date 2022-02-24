dd749c8702a4cdce7b2530083b39a9e1dfd74187
Martina Sirotic <sirotic.martina@gmail.com>
2022-02-24 12:11:00 +0100

    popravak

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 from django.db import models
 from PIL import Image
 # Create your models here.
@@ -22,6 +23,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.user
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Post(models.Model):
     profile_post = models.ForeignKey(Profile, on_delete = models.CASCADE)
